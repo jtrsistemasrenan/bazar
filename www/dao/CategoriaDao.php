60c4923b48947377be10692c5deabe50acfdc45c
root <renanhome-IPMH61R1>
2014-11-09 22:28:10 -0200

    implementação dos metodos da interface

--- www/dao/CategoriaDao.php
+++ www/dao/CategoriaDao.php
@@ -6,7 +6,30 @@
  * Time: 18:16
  */
 
-include_once("iDao.php")
+include_once("iDao.php");
 class CategoriaDao implements iDao {
-    private $nome;
-} 
+    public function criar($obj)
+    {
+        // TODO: Implement criar() method.
+    }
+
+    public function remover($obj)
+    {
+        // TODO: Implement remover() method.
+    }
+
+    public function atualizar($objAntigo, $objNovo)
+    {
+        // TODO: Implement atualizar() method.
+    }
+
+    public function listar()
+    {
+        // TODO: Implement listar() method.
+    }
+
+    public function buscar($objId)
+    {
+        // TODO: Implement buscar() method.
+    }
+}
